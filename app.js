f085027ba5c2f7f22c46e39cf979c67ae26b5b2d
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-01-22 17:18:16 +0300

    add: simple scene

---
+++ app.js
@@ -0,0 +1,21 @@
+import * as THREE from "https://unpkg.com/three@0.136.0/build/three.module.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    innerWidth / innerHeight,
+    0.1,
+    1000
+)
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const box = new THREE.BoxGeometry(1, 1, 1);
+const mat = new THREE.MeshBasicMaterial({ color:0x00ff00})
+const mesh = new THREE.Mesh(box, mat);
+scene.add(mesh);
+camera.position.z = 5;
+renderer.render(scene, camera);
+

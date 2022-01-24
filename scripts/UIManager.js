4c7b8ecc6b98c776c2bd924c5a943d147d3b21e7
ENSAR TOPUZ <eensar.topuz@gmail.com>
2022-01-24 23:07:42 +0300

    :/

--- scripts/UIManager.js
+++ scripts/UIManager.js
@@ -5,7 +5,7 @@ let body = document.body,
     info = document.getElementById("info"),
     message = document.getElementById("message");
 
-setMessageWidth();
+adjustMessageContent();
 message.style.visibility = 'visible';
 
 window.addEventListener('resize', function(){
@@ -14,26 +14,52 @@ window.addEventListener('resize', function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     info.style.width = '100%';
     info.style.height = '100%';
-    setMessageWidth();
+    adjustMessageContent();
+    console.log(getPixelRatio());
 }, false);
 
 
-function setMessageWidth(){
-    if(calculatePixelRatio() < 0.90){
+function adjustMessageContent(){
+
+    if (getPixelRatio() < 0.5){
+        message.style.fontSize = '100%';
+        message.style.width = "95%";
+    }
+
+    else if(getPixelRatio() < 1.0){
+        message.style.fontSize = '115%';
         message.style.width = "90%";
     }
-    else{
-        message.style.width =  '40%';
+
+    else if(getPixelRatio() < 1.2){
+        message.style.fontSize = '120%';
+        message.style.width =  '75%';
     }
+
+    else if (getWidth() > 1024){
+        message.style.fontSize = '130%';
+        message.style.width =  '55%';
+    }
+
+    else{
+        message.style.fontSize = '120%';
+        message.style.width =  '80%';
+    }
+
 }
 
-function calculatePixelRatio(){
-    let height = Math.max( body.scrollHeight, body.offsetHeight,
+
+function getPixelRatio(){
+    return getWidth() / getHeight();
+}
+
+function getHeight(){
+    return Math.max( body.scrollHeight, body.offsetHeight,
         html.clientHeight, html.scrollHeight, html.offsetHeight);
+}
 
-    let width = Math.max( body.scrollWidth, body.offsetWidth,
+function getWidth(){
+    return Math.max( body.scrollWidth, body.offsetWidth,
         html.clientWidth, html.scrollWidth, html.offsetWidth);
-
-    return width / height;
 }
 
